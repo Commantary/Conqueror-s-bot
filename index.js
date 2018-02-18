97d4a02069f2ebced47a8def52de21b89290f618
Unknown <babathiste@gmail.com>
2018-02-18 21:47:01 +0100

    Added Files

--- index.js
+++ index.js
@@ -39,17 +39,6 @@ client.on('message', message => {
   } catch (err) {
     console.error(err)
   }
-
-  fs.readdir('./events/', (err, files) => {
-  if (err) return console.error(err)
-  files.forEach(file => {
-    let eventFunction = require(`./events/${file}`)
-    let eventName = file.split('.')[0]
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
-    client.on(eventName, (...args) => eventFunction.run(client, ...args))
-  })
-})
-
 })
 
 var dt = process.env.TOKEN || process.argv[2]
