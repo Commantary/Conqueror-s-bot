a59eb5cecb857bd225be72f9071b86fcd85803b2
Unknown <babathiste@gmail.com>
2018-02-22 17:32:50 +0100

    Added Files

--- module/help.js
+++ module/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 
 		message.channel.send({embed: {
 		color: 11133683,
-		description: "=profil [pseudo sans maj]\n=add [planete/armee] [montant] [pseudo sans maj]\n=remove [planete/armee] [montant] [pseudo sans maj]"
+		description: "=profil [pseudo sans maj]\n=add [planete/armee] [montant] [pseudo sans maj]\n=remove [planete/armee] [montant] [pseudo sans maj]\n=list"
 		}})
 
 	}
