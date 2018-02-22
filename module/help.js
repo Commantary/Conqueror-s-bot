872213df235ba75f31489109c5ff639685fe6cac
Unknown <babathiste@gmail.com>
2018-02-22 17:16:44 +0100

    Added Files

--- module/help.js
+++ module/help.js
@@ -3,10 +3,19 @@ const fs = require('fs')
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-	
-	message.channel.send({embed: {
+	if(args[0]==="power"){
+
+		message.channel.send({embed: {
+		color: 11133683,
+		description: "0: Aucun\n1: Sphère du Mystère\n2: Sphère du Renseignement Impérial\n3: Sphère des Sciences Biotiques\n4: Sphère de la Technologie\n5: Sphère de la Diplomatie\n6: Sphère de la Défense de l'Empire\n7: Sphère de l'Offensive Militaire\n8: Sphère de la Stratégie Militaire\n9: Sphère de la Philosophie\na: Sphère du Savoir Ancestral\nb: Sphère de la Justice\nc: Sphère de la Logistique & Production"
+		}})
+
+	} else {
+
+		message.channel.send({embed: {
 		color: 11133683,
 		description: "=profil [pseudo sans maj]\n=add [planete/armee] [montant] [pseudo sans maj]\n=remove [planete/armee] [montant] [pseudo sans maj]"
-	}})
+		}})
 
+	}
 } // FIN DU MODULE EXPORTS
---
+++ module/list.js
@@ -0,0 +1,42 @@
+const request = require('request')
+const fs = require('fs')
+const Discord = require('discord.js')
+var bdd = process.env.BDD || process.argv[2]
+
+module.exports.run = async (client, message, args) => {
+	// LES VARIABLES
+	var array = []
+	a = 0
+	b = 0
+	c = 1
+
+	request.get(bdd, function (err, res, body) {
+		var data = JSON.parse(body)
+		var teste = body.split("!")
+		console.log(teste[1])
+
+		for(i=0;i<teste.length;i++){
+				i++
+				array[a] = "[" + c + "] " + teste[i]
+				console.log(array[a] + "|" + teste[i] + "|")
+				a++
+				c++
+		}
+		a--
+		c--
+		array[a] = "[" + c + "] ?????"
+		message.channel.send({embed: {
+			color: 11133683,
+			author: {
+	            name: 'Liste des joueurs'
+	          },
+			description: array.join('\n')
+		}})
+	})
+
+	// RESET DES VARIABLES
+	a = 0
+	b = 0
+	c = 1
+
+} // FIN DU MODULE EXPORTS
