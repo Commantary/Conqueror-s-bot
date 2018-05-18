536a7d800d32ef02298567bb4b3d3e2649fafd64
Commantary <31055935+Commantary@users.noreply.github.com>
2018-05-18 20:18:16 +0200

    Update argent.js

--- module/argent.js
+++ module/argent.js
@@ -2,8 +2,8 @@ const request = require('request')
 const fs = require('fs')
 const Discord = require('discord.js')
 var config = require("./config.json")
-var bdd = config.bdd
-var bdd_number = config.bdd_number
+var bdd = process.env.BDD || process.argv[2]
+var bdd_number = process.env.BDDNUMBER || process.argv[2]
 
 module.exports.run = (client) => {
 	// LES VARIABLES
